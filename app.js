15fa1d5a0a7eeaae392a06916b7f942363834801
alejovarcasia <alejovarcasia@gmail.com>
2021-08-11 22:46:30 -0300

    agrenado puerto

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const puerto = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/index.html"));
