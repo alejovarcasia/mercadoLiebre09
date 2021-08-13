50410d1f1d72581fff4766f145dd296385c7a9d6
alejovarcasia <alejovarcasia@gmail.com>
2021-08-12 22:53:05 -0300

    cambios

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const puerto = process.env.PORT || 3000;
+const port = process.env.PORT || '3000';
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on PORT : 3000");
-});
+app.listen(port,()=>{
 
-app.use(express.static('public'));
+  console.log('Server is runnig in the Port : ${port}');
