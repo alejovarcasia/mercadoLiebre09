9f44be4c38c41c584bd24e2d1c89826ca2d07c29
Alejo Varcasia <alejovarcasia@gmail.com>
2021-10-05 00:40:42 -0300

    adding create and login

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ app.use(express.static(path.join(__dirname, './public')));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/index.html"));
 });
+app.get("/create",(req,res)=>{
+  res.sendFile(path.join(__dirname, "./view/create.html"))
+})
+app.get("/login",(req,res)=>{
+  res.sendFile(path.join(__dirname, "./view/login.html"))
+})
 
 app.listen(puerto, () => console.log(`escuchando en el puerto ${puerto}`));
